980b5bacb218259cc16e9567740e0c433e9b0b9f
aristata <aristatait@gmail.com>
2024-02-15 16:55:40 +0900

    feat: 서버 사이드 fetch

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,23 @@
-export const metadata = {
+import { Metadata } from "next";
+
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+export const metadata: Metadata = {
   title: "Home"
 };
 
-export default function Tomato() {
+async function getMovies() {
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
       <h1>Hello!</h1>
+      {JSON.stringify(movies)}
     </div>
   );
 }
