db36a44f095a872cb15379d77b4227c65c3a3e75
aristata <aristatait@gmail.com>
2024-02-16 16:36:48 +0900

    feat: 병렬 조회

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,15 +1,44 @@
-export default function MovieDetail({
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({
   params: { id },
   searchParams: { v, page }
 }: {
   params: { id: string };
   searchParams: { v: string; page: string };
 }) {
+  console.log("==============================");
+  console.log("fetch 시작");
+  // const movie = await getMovie(id);
+  // const vidoes = await getVideos(id);
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  // console.log(videos);
+  console.log("fetch 종료");
   return (
     <>
       <h1>Movie {id}</h1>
       <p>v = {v}</p>
       <p>page = {page}</p>
+      <p>{movie.title}</p>
+      <p>
+        {videos.map((v) => (
+          <li key={v.id}>{v.name}</li>
+        ))}
+      </p>
     </>
   );
 }
