5531bf115916be46ac4ecd1ef66626514dc428cc
aristata <aristatait@gmail.com>
2024-02-20 10:48:34 +0900

    에러 처리

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ErrorPage() {
+  return (
+    <>
+      <h1>에러가 나타났다!!!</h1>
+    </>
+  );
+}
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -3,8 +3,9 @@ import { API_URL } from "../app/(home)/page";
 async function getVideos(id: string) {
   console.log(`Fetching videos: ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
+  throw new Error("비디오를 조회하는 중에 에러가 발생하였습니다.");
+  // const response = await fetch(`${API_URL}/${id}/videos`);
+  // return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
