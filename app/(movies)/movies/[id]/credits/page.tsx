3e080230d60ca42ba2f55ad39165b017fcbff737
aristata <aristatait@gmail.com>
2024-02-27 11:56:23 +0900

    feat: credit 타이틀 및 돌아가기 추가

--- app/(movies)/movies/[id]/credits/page.tsx
+++ app/(movies)/movies/[id]/credits/page.tsx
@@ -1,12 +1,15 @@
 import getCredits from "@/apis/getCredits";
 import style from "@/components/movie/movie-credit.module.css";
+import Link from "next/link";
 
 export default async function CreditPage({ params: { id } }) {
   const credits = await getCredits(id);
 
   return (
-    <>
-      <div className={style.container}>
+    <div className={style.container}>
+      <h1 className={style.title}>Credits</h1>
+      <Link href={`/movies/${id}`}>👈 돌아가기</Link>
+      <div className={style.creditsGrid}>
         {credits.map((credit) => (
           <div key={credit.id} className={style.item}>
             <img src={credit.profile_path}></img>
@@ -24,7 +27,8 @@ export default async function CreditPage({ params: { id } }) {
             </div>
           </div>
         ))}
+        L
       </div>
-    </>
+    </div>
   );
 }
